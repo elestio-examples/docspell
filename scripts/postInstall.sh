ddf586f3d96fec3217abfe81381de7cc657e062d
AmsellemJoseph <amsellem.joseph@gmail.com>
2022-10-26 16:42:46 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -18,7 +18,7 @@ curl http://$target/api/v1/open/signup/register \
   -H 'Referer: https://docspell2-u353.vm.elestio.app/app/login?r=/app/item/6XcBB5zgrWg-JUUS6MZFXv4-Ez5FsN7XpKa-i9HBw44iyZQ&openid=0' \
   -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36' \
   -H 'Content-Type: application/json' \
-  --data-raw '{"account":"root","password":"'"${ADMIN_PASSWORD}"'","rememberMe":false}' \
+  --data-raw '{"account":"root","password":"'"${ADMIN_PASSWORD}"'","rememberMe":true}' \
   --compressed)
 
   echo "jwt " $jwt
@@ -33,5 +33,5 @@ curl http://$target/api/v1/open/signup/register \
   -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36' \
   -H 'X-Docspell-Auth: '"${token}"'' \
   -H 'sec-ch-ua-platform: "macOS"' \
-  --data-raw '{"name":"rootmail","smtpHost":"'"${EMAIL_HOST}"'","smtpPort":'"${EMAIL_PORT}"',"smtpUser":'"${EMAIL_HOST_USER}"',"smtpPassword":'"${EMAIL_HOST_PASSWORD}"',"from":"y","replyTo":null,"sslType":"none","ignoreCertificates":true}' \
+  --data-raw '{"name":"rootmail","smtpHost":'"${EMAIL_HOST}"',"smtpPort":'"${EMAIL_PORT}"',"smtpUser":'"${EMAIL_HOST_USER}"',"smtpPassword":'"${EMAIL_HOST_PASSWORD}"',"from":'"${DEFAULT_FROM_EMAIL}"',"replyTo":null,"sslType":"none","ignoreCertificates":true}' \
   --compressed
