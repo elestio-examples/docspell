2458c18198ac56d05bd3217cf726f0487d004912
Prem chavhan <pchavhan@logiquad.com>
2023-11-01 16:35:50 +0530

    fixes

--- tests/joex/build.sh
+++ tests/joex/build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 GIT_VERSION=$(curl -s https://api.github.com/repos/eikek/docspell/releases/latest | grep tag_name | cut -d '"' -f 4)
 GIT_VERSION=${GIT_VERSION#v}
-cp -rf ./docker/dockerfiles/* ./
+cp docker/dockerfiles/joex.dockerfile ./
 mv joex.dockerfile Dockerfile
+cp docker/dockerfiles/joex-entrypoint.sh ./
 sed -i "s~ARG version~ARG version=$GIT_VERSION~g" Dockerfile
 sed -i "s~pip3 install --upgrade pip~pip3 install --upgrade pip --break-system-packages~g" Dockerfile
 sed -i "s~pip3 install ocrmypdf~pip3 install ocrmypdf --break-system-packages~g" Dockerfile
--- tests/restserver/build.sh
+++ tests/restserver/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 GIT_VERSION=$(curl -s https://api.github.com/repos/eikek/docspell/releases/latest | grep tag_name | cut -d '"' -f 4)
 GIT_VERSION=${GIT_VERSION#v}
-cp -rf ./docker/dockerfiles/* ./
+cp docker/dockerfiles/restserver.dockerfile ./
 mv restserver.dockerfile Dockerfile
 sed -i "s~ARG version~ARG version=$GIT_VERSION~g" Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/docspell-restserver:latest | docker load
